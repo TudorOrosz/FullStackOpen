9a1126ac99cb24973af366ac68400ee41e37b491
Tudor Orosz <tudororosz@gmail.com>
2025-05-24 20:57:57 +0200

    1.9 - 1.11

--- part1/1.6->1.x/src/App.jsx
+++ part1/1.6->1.x/src/App.jsx
@@ -1,18 +1,44 @@
 import { useState } from 'react'
 
+// Component to display a line of statistics
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+// Component to display statistics based on feedback
 const Statistics = ({ good, neutral, bad }) => {
   console.log(good, neutral, bad)
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>average {(good - bad) / (good + neutral + bad)}</p>
-    <p>positive {good / (good + neutral + bad) * 100} %</p>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value ={good} />
+          <StatisticLine text="neutral" value ={neutral} />
+          <StatisticLine text="bad" value ={bad} />
+          <StatisticLine text="all" value ={good + neutral + bad} />
+          <StatisticLine text="average" value ={(good - bad) / (good + neutral + bad)} />
+          <StatisticLine text="positive" value ={(good / (good + neutral + bad) * 100) + ' %'} />
+          </tbody>
+      </table>
     </div>
   )}
+
+// Component for a button that can be reused
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -23,10 +49,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
-      
+      <Button onClick={() => setGood(good + 1)} text = 'good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text = 'neutral' />
+      <Button onClick={() => setBad(bad + 1)} text = 'bad' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
