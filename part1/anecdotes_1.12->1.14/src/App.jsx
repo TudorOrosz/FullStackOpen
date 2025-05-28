0ad898ef17e872555ef8c133ae924e4f3be9c417
Tudor Orosz <tudororosz@gmail.com>
2025-05-28 21:09:47 +0200

    Added anecdotes 1.12->1.14

--- part1/anecdotes_1.12->1.14/src/App.jsx
+++ part1/anecdotes_1.12->1.14/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,12 +17,41 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+  
+  // State to keep track of the selected anecdote and votes
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  
+      // Handler for next anecdote
+  const handleNextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length)) //setSelected(number 0 -> 7)
+  }
 
+    // Handler for voting
+  const handleVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+  console.log(votes)
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <div>
+        {anecdotes[selected]}
+      </div>
+      <div>
+        <p>has {votes[selected]} votes</p>
+        <Button onClick={handleVote} text="vote" />
+        <Button onClick={handleNextAnecdote} text="next anecdote" />
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <div>
+          {anecdotes[votes.indexOf(Math.max(...votes))]}
+        </div>
+        <p>has {Math.max(...votes)} votes</p>
+      </div>
+      </div>
     </div>
   )
 }
