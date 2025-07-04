9224b014421e3e177783b3dc4e6b8c75323eb28a
Tudor Orosz <tudororosz@gmail.com>
2025-07-04 13:37:18 +0200

    3.15 -> 3.18: I cahnged the structure of all the calls to reference MongoDB database info

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -7,14 +7,17 @@ const app = express()
 
 let persons = []
 
+// First Middleware specifications
 morgan.token('content', function (req) {
   return JSON.stringify(req.body)
 })
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
-app.use(express.static('dist'))
+// First Middleware specification - DONE
 
+//// API CALLS
 // CALL: Full list of persons
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -24,29 +27,36 @@ app.get('/api/persons', (request, response) => {
 
 // CALL: Info of list of persons
 app.get('/info', (request, response) => {
-    const time = new Date()    
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
+  const time = new Date()
+  Person.countDocuments({})
+    .then(count => {
+      response.send(
+        `<p>Phonebook has info for ${count} people</p>
          <p>${time}</p>`
-    )
+      )
+    })
 })
 
 // CALL: Get person by ID
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // CALL: Delete person by ID
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(person => {
-    response.status(204).end()
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // CALL: Create person
@@ -59,21 +69,68 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
+  Person.findOne({ name: body.name })
+    .then(existingPerson => {
+      if (existingPerson) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+      }
+
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+
+      person.save()
+        .then(savedPerson => {
+          response.json(savedPerson)
+        })
+        .catch(error => next(error))
     })
-  }  
-
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+    .catch(error => next(error))
 })
+
+// CALL: Update a person's data
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+
+// Last part of middleware -> unknownEndpoint + errorHandler
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+// Middleware specification - DONE
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
