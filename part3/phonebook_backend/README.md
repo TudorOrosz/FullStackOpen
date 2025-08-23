eeecc2a46c3fe61e859a5cf8fcdeff7e0165ed2b
Tudor Orosz <tudororosz@gmail.com>
2025-08-23 15:37:13 +0200

    uibuild

--- part3/phonebook_backend/README.md
+++ part3/phonebook_backend/README.md
@@ -5,3 +5,7 @@ This will launch the whole application on http://localhost:3001/ . Everything is
 ## Deploying to web:
 Link to Production version of application, deployed on render: https://fullstackopen-xq5q.onrender.com/
 To deploy app to render, run: 'npm run deploy:full'
+
+## Bringing changes from frontend folder to backend folder by copying dist file
+If you made changes to frontend folder and want to bring them to backend to test full functionality of the app:
+run: 'npm run build:ui' and then open the app: 'node --watch index.js'
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -60,7 +60,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 // CALL: Create person
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -124,7 +124,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
