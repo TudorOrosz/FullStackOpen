d3f1aa14aa4d4b7c95cfcb1278393a6f311b7374
Tudor Orosz <tudororosz@gmail.com>
2025-10-24 16:16:26 +0200

    Up until 4.12 incl done

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -6,21 +6,18 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   const blog = new Blog({
-    title: body.content,
+    title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
+    likes: body.likes
   })
 
-  blog.save()
-    .then(savedBlog => {
-      response.status(201).json(savedBlog)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -37,14 +37,54 @@ test('unique identifier property of blog posts is named id', async () => {
   })
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "test Title",
+    author: "test Author",
+    url: "www.test.com",
+    likes: 7,
+  }
 
-// For now we do not need this test in the exercises
-// test('a specific blog is within the returned notes', async () => {
-//   const response = await api.get('/api/blogs')
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
 
-//   const titles = response.body.map((e) => e.title)
-//   assert(titles.includes('React patterns'))
-// })
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+  assert(titles.includes('test Title'))
+})
+
+test('if like property is missing from the request, it will default to 0', async () => {
+  const newBlog = {
+    title: "test Title 2",
+    author: "test Author 2",
+    url: "www.test.com 2"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const testBlog = response.body.filter(r => r.title === "test Title 2")
+  
+  assert.strictEqual(testBlog[0].likes, 0)
+})
+
+test('if the title or url properties are missing from the request, the backend responds with status code 400', async () => {
+  const missingUrl = { title: 'test Title 3', author: 'test Author 3', likes: 7 }
+  const missingTitle = { author: 'test Author 4', url: 'www.test.com 4', likes: 7 }
+
+  for (const blog of [missingUrl, missingTitle]) {
+    await api.post('/api/blogs').send(blog).expect(400)
+  }
+})
 
 after(async () => {
   await mongoose.connection.close()
