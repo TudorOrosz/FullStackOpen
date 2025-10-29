1e97833108bcf15e07610c91f39937af71ec2e07
Tudor Orosz <tudororosz@gmail.com>
2025-10-29 09:05:14 +0100

    FIxed the runnning of tests, using npm test now instead of node test

--- part4/blog_list/tests/user_tests.test.js
+++ part4/blog_list/tests/user_tests.test.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
-const { xor } = require('lodash')
 
 const api = supertest(app)
 
